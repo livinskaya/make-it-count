b7bc546ddfb765a5c54fe849dd2d1d9cbd246022
Lando Winzen <winzenlando@gmail.com>
2025-09-24 12:55:59 +0000

    Version 1 - count on all Buttons

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,18 +1,40 @@
+import { useState } from 'react'
 import './App.css'
-function MyButton() {
-  return (
-    <button className='bg-amber-100 rounded-md'>Click me</button>
-  )
-}
 
 
 function App() {
 
+  const [count, setCount] = useState(0);
+
+  function Click() {
+    setCount(count + 1);
+  }
+
   return (
     <>
-      <MyButton />
+
+      <div className="flex flex-col justify-center items-center h-screen bg-gray-700">
+        <h1 className="mb-11 text-6xl text-white">Make it Count!</h1>
+        <div className="flex flex-col gap-20 items-center">
+          <Button name={"app count is "} count={count} onClick={Click} />
+          <div className="flex flex-row gap-15">
+            <Button name={"count is "} count={count} onClick={Click} />
+            <Button name={"count is "} count={count} onClick={Click} />
+
+          </div>
+        </div>
+
+      </div>
     </>
   )
+
+  function Button({ name, count, onClick }) {
+    return <button className='bg-gray-500 text-white rounded-lg p-1' onClick={onClick}>
+      {name}{count}
+    </button>
+  }
 }
+
+
 
 export default App
