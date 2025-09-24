e75f3191671c33615f7d8b53ce4b65324be700b1
Lando Winzen <winzenlando@gmail.com>
2025-09-24 06:14:09 +0000

    first commit

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+function MyButton() {
+  return (
+    <button className='bg-amber-100 rounded-md'>Click me</button>
+  )
+}
+
+
+function App() {
+
+  return (
+    <>
+      <MyButton />
+    </>
+  )
+}
+
+export default App
