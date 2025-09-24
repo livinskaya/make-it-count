ae1473a0a5f5568d09a3bfe66aab46671c052770
Lando Winzen <winzenlando@gmail.com>
2025-09-24 07:34:04 +0000

    Styling tests with tailwind

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,7 +1,16 @@
 import './App.css'
-function MyButton() {
+
+function Button() {
   return (
-    <button className='bg-amber-100 rounded-md'>Click me</button>
+    <button className='bg-red-500 rounded-md  hover:bg-amber-200 focus:outline-2 focus:outline-offset-1 focus: outline-amber-500'></button>
+  )
+}
+
+function Thumbnail() {
+  return (
+    <div className='pt-15 pl-25 rounded-md bg-blue-400 relative top-5 left-3'>
+
+    </div>
   )
 }
 
@@ -10,7 +19,16 @@ function App() {
 
   return (
     <>
-      <MyButton />
+      <div className="flex items-center h-screen justify-center gap-3 bg-blue-200 flex-col">
+        <h1 className="mb-5 font-semibold text-4xl absolute top-80">Styling</h1>
+        <div className="bg-white rounded-2xl w-120 h-25">
+          <div className="flex flex-row">
+            <Thumbnail />
+            <h2></h2>
+            <Button />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
