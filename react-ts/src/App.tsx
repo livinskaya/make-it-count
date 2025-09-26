ba3e681df809bacb6878c0fb7ed188ef651b251d
Lando Winzen <winzenlando@gmail.com>
2025-09-26 07:37:40 +0000

    Count finished

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,38 +1,52 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 
 function App() {
 
-  const [count, setCount] = useState(0);
+  const [counters, setCount] = useState([0, 0, 0]);
 
-  function Click() {
-    setCount(count + 1);
+  // effect combi interval
+
+  function countAll() {
+    setCount(counters.map((count) => count + 1))
   }
+
+  function countOne(index: number) {
+    setCount(counters.map((count, i) => (i === index ? count + 1 : count)))
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(counters.map((count) => count + 1))
+    }, 2000);
+
+    return () => clearInterval(interval)
+  })
+
 
   return (
     <>
 
       <div className="flex flex-col justify-center items-center h-screen bg-gray-700">
-        <h1 className="mb-11 text-6xl text-white">Make it Count!</h1>
-        <div className="flex flex-col gap-20 items-center">
-          <Button name={"app count is "} count={count} onClick={Click} />
-          <div className="flex flex-row gap-15">
-            <Button name={"count is "} count={count} onClick={Click} />
-            <Button name={"count is "} count={count} onClick={Click} />
+        <div className="">
+          <h1 className="mb-11 text-6xl text-white">Make it Count!</h1>
+          <div className="flex flex-col gap-20 items-center">
+            <button onClick={countAll} className='bg-gray-500 text-white rounded-lg p-1'>app count is {counters[0]}</button>
+            <div className="flex flex-row gap-15">
+              <button onClick={() => countOne(1)} className='bg-gray-500 text-white rounded-lg p-1'>
+                count is {counters[1]} </button>
+              <button onClick={() => countOne(2)} className='bg-gray-500 text-white rounded-lg p-1'>
+                count is {counters[2]}</button>
 
+            </div>
           </div>
         </div>
-
-      </div>
+      </div >
     </>
   )
 
-  function Button({ name, count, onClick }) {
-    return <button className='bg-gray-500 text-white rounded-lg p-1' onClick={onClick}>
-      {name}{count}
-    </button>
-  }
+
 }
 
 
